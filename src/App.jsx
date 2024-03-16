b66008c6142fccefdfbb46fa5a1e346cd6f6aba5
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-03-16 08:34:52 +0530

    firstInit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PercentageWheel from './components/PercentageWheel';
+import { DownOutlined } from '@ant-design/icons';
+import Calendar from './components/Calendar';
+const App = () => (
+  <>
+   <div className='flex justify-center h-full items-center'>
+     <Calendar/>
+  </div>
+  </>
+);
+export default App;
